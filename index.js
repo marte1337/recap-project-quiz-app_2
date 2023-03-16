23c026d1e6041ca9f22d5fc5d0280826cf9b00bb
Marte Auer <marte_auer@hotmail.com>
2023-03-16 15:45:16 +0100

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// phew… not a lot going on here. Please add some code!
+console.clear()
+
+const buttonBookmark = document.querySelector('[data-js="buttonBookmark"]')
+
+const cardButtonAnswer = document.querySelector('[data-js="cardButtonAnswer"]')
+const cardAnswer = document.querySelector('[data-js="cardAnswer"]')
+
+buttonBookmark.addEventListener("click", () => {
+    buttonBookmark.classList.toggle("bookmark--active")
+})
+
+
+cardButtonAnswer.addEventListener("click", () => {
+    cardAnswer.classList.toggle("card__answer--active")
+
+    console.log(cardButtonAnswer.textContent)
+
+    if (cardButtonAnswer.textContent.trim() === "Show answer") {
+        cardButtonAnswer.textContent = "Hide answer"
+    } else {
+        cardButtonAnswer.textContent = "Show answer"
+    }
+
+    console.log(cardButtonAnswer.textContent)
+})
+
+
+
+
